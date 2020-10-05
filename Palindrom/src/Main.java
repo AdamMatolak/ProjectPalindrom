8fa74798b2a8c920b89617bd4576eb9b3ead7896
Adam Matoľák <adam.matolak@kosickaakademia.sk>
2020-10-05 12:28:55 +0200

    Palindrom commit

---
+++ Palindrom/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Zadaj čislo: ");
+        Scanner sc = new Scanner(System.in);
+
+        long x = sc.nextLong();
+        long x2 = x;
+        long y = 0;
+
+        while (x2 > 0) {
+            y = y + (x2 % 10);
+            y = y * 10;
+            x2 = (x2 / 10);
+        }
+        y = y / 10;
+        if(x==y){
+            System.out.println("Toto cislo je palindrom");
+        }
+        else{
+            System.out.println("Toto cislo neni palindrom");
+        }
+    }
+}
